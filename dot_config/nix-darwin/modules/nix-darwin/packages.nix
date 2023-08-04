fb8777ae7937ada232209813d27fef9e6ec68fca
Chad Pritchett <chad@chadpritchett.com>
2023-08-04 13:21:42 -0500

    Update .config/nix-darwin/modules/nix-darwin/packages.nix

--- dot_config/nix-darwin/modules/nix-darwin/packages.nix
+++ dot_config/nix-darwin/modules/nix-darwin/packages.nix
@@ -62,8 +62,7 @@
 
     # NOTE: Casks are prefered because of weird placement of GUI Apps by Nix / Nix Darwin
     casks = [
-      "alfred"
-      "microsoft-edge" 
+      "alfred" 
       "visual-studio-code"
       "logi-options-plus"
       "1password"
