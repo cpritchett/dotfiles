f9a9abb4a9c9320dce8ec8da652b194fcee5c559
Chad Pritchett <chad@chadpritchett.com>
2023-11-02 11:55:45 -0500

    Update .config/zsh/aliases/.eza.zsh Add scripts/Dracula.bbColorScheme.sh

--- dot_config/zsh/aliases/dot_eza.zsh
+++ dot_config/zsh/aliases/dot_eza.zsh
@@ -5,13 +5,15 @@
 if [ -x "$(command -v eza)" ]; then
     # general use
     alias ls='eza --group-directories-first'                                                          # ls
-    alias l='eza -lbF --git --group-directories-first'                                                # list, size, type, git
-    alias ll='eza -lbhGF --git --group-directories-first'                                             # long list
-    alias llm='eza -lbhGd --git --sort=modified'                            # long list, modified date sort
-    alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale --group-directories-first'  # all list
-    alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --group-directories-first' # all + extended list
+    alias l='eza -lbF --git --icons --group-directories-first'                                                # list, size, type, git
+    alias ll='eza -lbhGF --git --icons --group-directories-first'                                             # long list
+    alias llm='eza -lbhGd --git --icons --sort=modified'                            # long list, modified date sort
+    alias la='eza -lbhHigUmuSa --icons --time-style=long-iso --git --color-scale --group-directories-first'  # all list
+    alias lx='eza -lbhHigUmuSa@ --icons --time-style=long-iso --git --color-scale --group-directories-first' # all + extended list
+
+    alias xl='exa -la --icons --color=always'
 
     # specialty views
-    alias lS='eza -1 --group-directories-first'                                                              # one column, just names
-    alias lt='eza --tree --level=2 --group-directories-first'                                         # tree
+    alias lS='eza -1 --icons --group-directories-first'                                                              # one column, just names
+    alias lt='eza --tree --icons --level=2 --group-directories-first'                                         # tree
 fi
